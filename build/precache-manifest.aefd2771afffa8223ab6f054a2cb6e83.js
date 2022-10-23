e615eb8024b8e144f428d832a5e1b83020b899c5
Ibrahim Elmi <elmiibrahim@gmail.com>
2022-10-23 18:10:46 -0400

    github action heroku deploy script added

---
+++ build/precache-manifest.aefd2771afffa8223ab6f054a2cb6e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78946f0153d8c051c1f9",
+    "url": "/Banu500/comp3104/static/css/main.73ee8745.chunk.css"
+  },
+  {
+    "revision": "78946f0153d8c051c1f9",
+    "url": "/Banu500/comp3104/static/js/main.78946f01.chunk.js"
+  },
+  {
+    "revision": "86e69d289653d44f33f5",
+    "url": "/Banu500/comp3104/static/js/runtime~main.86e69d28.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Banu500/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "87a2e5a9d0ab861c5a453932d9ec1f31",
+    "url": "/Banu500/comp3104/index.html"
+  }
+];
